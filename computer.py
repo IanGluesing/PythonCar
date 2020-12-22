eec1e593e1f8f3b77703cbdc4e5d51d9b9092186
IanGluesing <ianmsgames@gmail.com>
2020-12-21 22:57:39 -0600

    Track orbs/features between frames

--- computer.py
+++ computer.py
@@ -5,22 +5,52 @@ import struct
 from PIL import Image
 import numpy as np
 import cv2
+import math
 
 def drawMethod(cv2Image):
 	#Method to draw a cv2 image to the screen
 	cv2.imshow('Window', cv2.cvtColor(cv2Image, cv2.COLOR_BGR2RGB))
 	cv2.waitKey(1)
 
-def orbMethod(image):
+def match(i, lastI):
+	#Method to match keypoints between frames
+	if np.array(lastI.des).any():
+		# Create match finder
+		bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+		#Find matches using descriptors from current frame and last frame
+		matches = bf.match(i.des, lastI.des)
+		#Sort matches
+		matches = sorted(matches, key = lambda x:x.distance)
+		
+		pts1 = []
+		pts2 = []
+		# Get keypoints from first and second image that were tracked
+		for dmatch in matches:
+				pts1.append(i.kp[dmatch.queryIdx].pt)
+				pts2.append(lastI.kp[dmatch.trainIdx].pt)
+
+		# Loop through these two sets of keypoints
+		for pt1, pt2, in zip(pts1, pts2):
+			# Get x,y coords for each of the two points
+			x1, y1 = pt1[0], pt1[1]
+			x2, y2 = pt2[0], pt2[1]
+			#Draw a line from the previous image point to the current image point
+			if(math.sqrt((x1-x2)**2 + (y1-y2)**2) < 50):
+				cv2.line(i.image, (int(x1), int(y1)), (int(x2), int(y2)), color=(255,0,0), thickness = 2)
+
+
+	return i
+
+def orbMethod(im):
 	#Create cv2s orb feature that tracks 2000 features
-	orb = cv2.ORB_create(nfeatures=2000)
+	orb = cv2.ORB_create(nfeatures=250)
 	#Get key points in the image
-	kp = orb.detect(image, None)
+	im.kp = orb.detect(im.image, None)
 	#Compute the keypoints and desciptions
-	kp, des = orb.compute(image, kp)
+	im.kp, im.des = orb.compute(im.image, im.kp)
 	#Draw the keypoints to the image
-	imgNew = cv2.drawKeypoints(image, kp, outImage = None, color =(0,255,0), flags = 0)
-	return imgNew
+	im.image = cv2.drawKeypoints(im.image, im.kp, outImage = None, color =(0,255,0), flags = 0)
+	return im
 
 def harrisCorner(image):
 	img = np.float32(image)
@@ -34,6 +64,13 @@ def harrisCorner(image):
 
 def pillowToNPArray(pillowImage):
 	return np.array(pillowImage)
+
+class frame():
+	def __init__(self, image):
+		self.orig = image
+		self.image = image
+		self.kp = [0,0,0]
+		self.des = [0,0,0]
 	
 
 def stream():
@@ -47,6 +84,7 @@ def stream():
 
 	try:
 		img = None
+		lastI = frame(None)
 		while True:
 			# Read the length of the image as a 32-bit unsigned int.
 			image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
@@ -70,10 +108,15 @@ def stream():
 			image = image.transpose(Image.ROTATE_180)
 			
 			#Convert to cv2 usable image
-			cv2Image = pillowToNPArray(image)
-			cv2Image = orbMethod(cv2Image)
+			i = frame(pillowToNPArray(image))
 
-			drawMethod(cv2Image)			
+			i = orbMethod(i)
+
+			i = match(i, lastI)
+
+			drawMethod(i.image)	
+			lastI = i
+			lastI.image = i.orig
 			
 	finally:
 		connection.close()
