3cb04ef64610e2af64e897e4a27629257399348b
Ian Gluesing <ianmsgames@gmail.com>
2025-07-01 17:33:45 -0500

    Initial split out of different processing threads

--- server/computer.py
+++ server/computer.py
@@ -6,62 +6,41 @@ import numpy as np
 import math
 from multiprocessing import Process, Queue
 
+from tracking.orb_extraction import process_frame_with_orb
+
 def drawLines(dmatches, i, lastI):
-	pts1 = []
-	pts2 = []
-	# Get keypoints from first and second image that were tracked
-	for dmatch in dmatches:
-			pts1.append(i.kp[dmatch.queryIdx].pt)
-			pts2.append(lastI.kp[dmatch.trainIdx].pt)
+    pts1 = []
+    pts2 = []
+    # Get keypoints from first and second image that were tracked
+    for dmatch in dmatches:
+            pts1.append(i.kp[dmatch.queryIdx].pt)
+            pts2.append(lastI.kp[dmatch.trainIdx].pt)
 
-	# Loop through these two sets of keypoints
-	for pt1, pt2, in zip(pts1, pts2):
-		# Get x,y coords for each of the two points
-		x1, y1 = pt1[0], pt1[1]
-		x2, y2 = pt2[0], pt2[1]
-		#Draw a line from the previous image point to the current image point
-		if(math.sqrt((x1-x2)**2 + (y1-y2)**2) < 50):
-			cv2.line(i.image, (int(x1), int(y1)), (int(x2), int(y2)), color=(255,0,0), thickness = 2)
+    # Loop through these two sets of keypoints
+    for pt1, pt2, in zip(pts1, pts2):
+        # Get x,y coords for each of the two points
+        x1, y1 = pt1[0], pt1[1]
+        x2, y2 = pt2[0], pt2[1]
+        #Draw a line from the previous image point to the current image point
+        if(math.sqrt((x1-x2)**2 + (y1-y2)**2) < 50):
+            cv2.line(i.image, (int(x1), int(y1)), (int(x2), int(y2)), color=(255,0,0), thickness = 2)
 
-	return i
-
+    return i
 
 def BFMatch(i, lastI):
-	#Method to match keypoints between frames
-	if np.array(lastI.des).any():
-		# Create match finder
-		bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-		#Find matches using descriptors from current frame and last frame
-		matches = bf.match(i.des, lastI.des)
-		#Sort matches
-		matches = sorted(matches, key = lambda x:x.distance)
+    #Method to match keypoints between frames
+    if np.array(lastI.des).any():
+        # Create match finder
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        #Find matches using descriptors from current frame and last frame
+        matches = bf.match(i.des, lastI.des)
+        #Sort matches
+        matches = sorted(matches, key = lambda x:x.distance)
 
-		# Draw the lines to the image using the brute force matches
-		drawLines(matches, i, lastI)
-		
-	return i
-
-def orbMethod(im):
-	#Create cv2s orb feature that tracks 2000 features
-	orb = cv2.ORB_create(nfeatures=2000)
-	#Get key points in the image
-	im.kp = orb.detect(im.image, None)
-	#Compute the keypoints and desciptions
-	im.kp, im.des = orb.compute(im.image, im.kp)
-	#Draw the keypoints to the image
-	#im.image = cv2.drawKeypoints(im.image, im.kp, outImage = None, color =(10,186,181), flags = 0)
-	return im
-
-def harrisCorner(image):
-	img = np.float32(image)
-	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	hcd_mg = cv2.cornerHarris(img_gray,5,5,0.08)
-	hcd_mg = cv2.dilate(hcd_mg, None)
-	image[hcd_mg > .01 * hcd_mg.max()] = [0,97,38]
-	#Uncomment to get rid of everything not considered a corner
-	image[hcd_mg <= .01 * hcd_mg.max()] = [0,0,0]
-	return image
-
+        # Draw the lines to the image using the brute force matches
+        drawLines(matches, i, lastI)
+        
+    return i
 
 class frame:
     def __init__(self, image):
@@ -92,7 +71,7 @@ def frame_processor(queue: Queue):
         i = frame(cv_img)
 
         # Optional image processing:
-        i = harrisCorner(i)
+        process_frame_with_orb(i)
         # i = BFMatch(i, lastI)
 
         drawMethod(i.image)
---
+++ server/tracking/orb_extraction.py
@@ -0,0 +1,12 @@
+import cv2
+
+def process_frame_with_orb(input_frame):
+    # Create orb detector
+    orb = cv2.ORB_create()
+    #Get key points in the image
+    input_frame.kp = orb.detect(input_frame.image, None)
+    #Compute the keypoints and desciptions
+    input_frame.kp, input_frame.des = orb.compute(input_frame.image, input_frame.kp)
+    #Draw the keypoints to the image
+    input_frame.image = cv2.drawKeypoints(input_frame.image, input_frame.kp, outImage = None, color =(10,186,181), flags = 0)
+    return input_frame
